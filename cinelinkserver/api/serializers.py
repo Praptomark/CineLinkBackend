8279c011687901b0d271204b3b56f6de52166164
Prapto Mark <51514572+Praptomark@users.noreply.github.com>
2023-12-05 00:11:52 +0600

    fixed login issue

--- cinelinkserver/api/serializers.py
+++ cinelinkserver/api/serializers.py
@@ -3,15 +3,9 @@ from .models import Movies, HallRoom, Seats, Schedules, Tickets, Cart, CartProdu
 from django.contrib.auth import models, get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = get_user_model()
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'is_superuser')
-
-    def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
-        return user
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
